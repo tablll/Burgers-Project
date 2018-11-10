b75b803005b9c2f0e3a108afb9a8a22c82b96f4e
Max Mirolyubov <maxim.mvm@gmail.com>
2018-11-10 23:17:23 +0300

    form responsive

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log("loaded");
+function openMenu() {
+    const menuItem = document.querySelectorAll(".menu__item");
+
+    for (i=0; i < menuItem.length; i++) {
+    
+        menuItem[i].addEventListener('click', function(){
+            this.classList.toggle('item__show');
+        });
+        menuItem[i].addEventListener('dblclick', function(){
+            this.classList.remove('item__show');
+        });
+    }  
+}
+
+openMenu();
