f26da9a55bee81bf7e312a9883fa2abea20f40b9
Max Mirolyubov <maxim.mvm@gmail.com>
2018-11-18 15:47:41 +0300

    fix for open-close logic in menu and team sections

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-// Open / close logic for section Team and Menu
+// Open---close logic for sections "Team" and "Menu"
 
 var menuSection = document.querySelectorAll(".menu__item");
 var menuCross = document.querySelectorAll(".menu__item-cross");
@@ -11,44 +11,56 @@ init ();
 function init () {
     openMenu(menuSection,'item__show');
     openMenu(teamSection,'show');
-}
+};
 
 function openMenu(item, itemClass) {
 
-    for (i=0; i < item.length; i++) {
+    for (let i=0; i < item.length; i++) {
         item[i].addEventListener('click', function(){
-            clearClass (item, itemClass);
-            this.classList.add(itemClass);
+            if (this.classList.contains(itemClass)) {
+                this.classList.remove(itemClass);
+            }
+            else {
+                clearClass (item, itemClass);
+                this.classList.add(itemClass);
+                }
         });
-        item[i].addEventListener('dblclick', function(){
-            clearClass (item, itemClass);
-        });
+    }
+};
 
-    }  
-}
+for (let i=0; i < menuCross.length; i++) {
+    menuCross[i].addEventListener('click', function(){
+            // removeClass(i);
+            clearClass(menuSection,'item__show');
+            event.stopPropagation();
+        })};
 
 function clearClass (item, itemClass) {
-    for (i=0; i < item.length; i++) {
+    for (let i=0; i < item.length; i++) {
         item[i].classList.remove(itemClass);
     }
-}
+};
 
-// End open / close
+// function removeClass (n) {
+//     menuSection[n].classList.remove("item__show");
+// };
 
-// Overlay for Reviews section
+// Overlay for "Reviews" section
 
 const openButton = document.querySelectorAll(".btn-overlay");
 const template = document.querySelector("#overlayTemplate").innerHTML;
 const overlay = createOverlay(template);
 const sectionReviews = document.querySelector('.reviews');
 const sectionReviewsContent = document.querySelectorAll('.reviews__text');
+const sectionReviewsTitle = document.querySelectorAll('.reviews__name');
 
 for (let i =0; i < openButton.length; i++) {
     openButton[i].addEventListener("click", function() {
     overlay.open();
+    overlay.setTitle(sectionReviewsTitle[i].textContent);
     overlay.setContent(sectionReviewsContent[i].textContent);
     });
-}
+};
 
 function createOverlay(template) {
   let fragment = document.createElement('div');
@@ -56,6 +68,7 @@ function createOverlay(template) {
   fragment.innerHTML = template;
 
   const overlayElement = fragment.querySelector(".overlay");
+  const contentTitle = fragment.querySelector(".overlay__title");
   const contentElement = fragment.querySelector(".overlay__content");
   const closeElement = fragment.querySelector(".overlay__close");
   
@@ -79,9 +92,13 @@ function createOverlay(template) {
     close() {
       closeElement.click();
     },
+    setTitle(content) {
+        contentTitle.innerHTML = content;
+    },
     setContent(content) {
       contentElement.innerHTML = content;
     }
   };
-}
+};
 
+// end of Reviews overlay 
